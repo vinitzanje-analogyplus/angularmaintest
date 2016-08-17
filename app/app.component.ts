815a0806b8a83561ae3562a377ec88e012346747
Vinit Zanje <Vinit Zanje>
2016-08-17 18:15:11 +0530

    k

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:    'my-app',
-  templateUrl: './html/app_component.html',
+  templateUrl: '/html/app_component.html',
 
 })
 export class AppComponent {
