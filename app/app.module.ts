d739e9efa152370a306026d66ba3974cdadf701c
Vinit Zanje <Vinit Zanje>
2016-08-17 19:25:10 +0530

    testing

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent }   from './app.component';
 import { AppLoginComponent }   from './login/app.login.component';
 
 import { routing }        from './app.routing';
+import { HttpModule }     from '@angular/http';
 
 
 
@@ -16,6 +17,7 @@ import { Httptest }  from './httpservice';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     routing
   ],
   declarations: [
