c8d049c8a9c06dde3fa3fac224f2e8053fef8a34
Vinit Zanje <Vinit Zanje>
2016-08-17 17:56:24 +0530

    test

--- systemjs.config.js
+++ systemjs.config.js
@@ -8,7 +8,7 @@
     'app':                        'app', // 'dist',
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    '@angular2-material': 'vendor/@angular2-material',
+    '@angular2-material': 'node_modules/@angular2-material',
     'rxjs':                       'node_modules/rxjs'
   };
   // packages tells the System loader how to load when no filename and/or no extension
